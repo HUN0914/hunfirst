b2be8414b66ff0ead9674c63cc867c4bc165b9c0
Hun0914 <bkh010914@naver.com>
2024-11-15 18:43:18 +0900

    refactor : 검색 기능 결과 반환 서비스 리팩토링

--- src/main/java/hello/hunfirst/service/RecruitService.java
+++ src/main/java/hello/hunfirst/service/RecruitService.java
@@ -1,5 +1,6 @@
 package hello.hunfirst.service;
 
+
 import hello.hunfirst.entity.Recruit;
 import hello.hunfirst.repository.RecruitRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ public class RecruitService {
 
     public List<Recruit> findAll(){return recruitRepository.findAll();};
 
-    public List<Recruit> findByTitle(String Title){return recruitRepository.findByTitle(Title);}
+    public List<Recruit> searchTitleContaining(String title){return recruitRepository.findByTitleContaining(title);};
 
     public Recruit findById(Long recruitId){
         return recruitRepository.findById(recruitId)
@@ -29,4 +30,6 @@ public class RecruitService {
     public void deleteById(Long recruitId) {
         recruitRepository.deleteById(recruitId);
     }
+
+
 }
