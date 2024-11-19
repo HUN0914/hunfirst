b9b81bfc9f34bf6570694a8d89b1f65256ef7cda
Hun0914 <bkh010914@naver.com>
2024-11-20 01:39:33 +0900

    feat : recruit 업데이트 로직 추가

--- src/main/java/hello/hunfirst/service/RecruitService.java
+++ src/main/java/hello/hunfirst/service/RecruitService.java
@@ -16,20 +16,39 @@ public class RecruitService {
 
     private final RecruitRepository recruitRepository;
 
-    public Recruit save(Recruit recruit){ return recruitRepository.save(recruit);};
+    public Recruit save(Recruit recruit) {
+        return recruitRepository.save(recruit);
+    }
 
-    public List<Recruit> findAll(){return recruitRepository.findAll();};
+    ;
 
-    public List<Recruit> searchTitleContaining(String title){return recruitRepository.findByTitleContaining(title);};
+    public List<Recruit> findAll() {
+        return recruitRepository.findAll();
+    }
 
-    public Recruit findById(Long recruitId){
+    ;
+
+    public List<Recruit> searchTitleContaining(String title) {
+        return recruitRepository.findByTitleContaining(title);
+    }
+
+    ;
+
+    public Recruit findById(Long recruitId) {
         return recruitRepository.findById(recruitId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 ID입니다. " + recruitId));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다. " + recruitId));
     }
 
     public void deleteById(Long recruitId) {
         recruitRepository.deleteById(recruitId);
     }
 
+    public void updateRecruit(Long recruitId, String title, String content, String startDate, String endDate, String favor) {
+
+        Recruit newRecruit=findById(recruitId);
+        newRecruit.updateRecruit(title, content, startDate, endDate, favor);
+        recruitRepository.save(newRecruit);
+
+    }
 
 }
